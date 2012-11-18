3b7ca1ace63b0905267199924a9a0758c9f70b77
Roman Shtylman <shtylman@gmail.com>
2012-11-17 21:33:38 -0500

    addEventListener should be called on the window object

--- index.js
+++ index.js
@@ -92,8 +92,8 @@
     if (running) return;
     running = true;
     if (false === options.dispatch) dispatch = false;
-    if (false !== options.popstate) addEventListener('popstate', onpopstate, false);
-    if (false !== options.click) addEventListener('click', onclick, false);
+    if (false !== options.popstate) window.addEventListener('popstate', onpopstate, false);
+    if (false !== options.click) window.addEventListener('click', onclick, false);
     if (!dispatch) return;
     page.replace(location.pathname + location.search, null, true, dispatch);
   };

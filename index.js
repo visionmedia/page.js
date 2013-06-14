1cb62cd4c3187536de1fcd49ee378a1c60b46be7
Nicolas Gryman <ngryman@gmail.com>
2013-06-14 18:43:13 -0400

    bower support.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-!(function() {
+;(function() {
 
   /**
    * Perform initial dispatch.
@@ -64,10 +64,10 @@
       for (var i = 1; i < arguments.length; ++i) {
         page.callbacks.push(route.middleware(arguments[i]));
       }
-      // show <path> with [state]
+    // show <path> with [state]
     } else if ('string' == typeof path) {
       page.show(path, fn);
-      // start [options]
+    // start [options]
     } else {
       page.start(path);
     }

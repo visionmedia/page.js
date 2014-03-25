e5dfc8a4806d772d3a8921ac74faddc2632e0e18
Ben Gourley <bn@grly.me>
2014-03-25 17:16:16 +0000

    Fix ctx.path not defaulting to '/' with empty path and #some-hash

--- index.js
+++ index.js
@@ -209,7 +209,7 @@
     this.hash = '';
     if (!~this.path.indexOf('#')) return;
     var parts = this.path.split('#');
-    this.path = parts[0];
+    this.path = parts[0] || '/';
     this.hash = parts[1] || '';
     this.querystring = this.querystring.split('#')[0];
   }

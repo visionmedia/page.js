afd036a365de87451e7381d8f36b2931be061038
Aaron Shafovaloff <aaronshaf@gmail.com>
2014-06-02 21:11:48 -0600

    pushState before dispatch
    Otherwise, window.location.pathname does not accurate representation current route upon dispatch

--- index.js
+++ index.js
@@ -123,8 +123,8 @@
 
   page.show = function(path, state, dispatch){
     var ctx = new Context(path, state);
-    if (false !== dispatch) page.dispatch(ctx);
     if (!ctx.unhandled) ctx.pushState();
+    if (false !== dispatch) page.dispatch(ctx);
     return ctx;
   };
 

d1a9296003fbdae225055ad0d81c04b047109843
Dmitrii Pakhtinov <Dmitrii.Pakhtinov@billing.ru>
2014-07-08 16:18:29 +0400

    added context

--- index.js
+++ index.js
@@ -120,8 +120,8 @@
 
   page.stop = function(){
     running = false;
-    removeEventListener('click', onclick, false);
-    removeEventListener('popstate', onpopstate, false);
+    window.removeEventListener('click', onclick, false);
+    window.removeEventListener('popstate', onpopstate, false);
   };
 
   /**

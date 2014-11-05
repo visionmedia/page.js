a7fcb21e34c41d8dd90c5a5b2cfe23ba32b966cb
Andy Burke <aburke@bitflood.org>
2014-11-05 02:46:11 -0800

    Remove unnecessary check.

--- index.js
+++ index.js
@@ -150,7 +150,7 @@
 
   page.show = function(path, state, dispatch){
     var ctx = new Context(path, state);
-    if (!ctx.unhandled) ctx.pushState();
+    ctx.pushState();
     if (false !== dispatch) page.dispatch(ctx);
     if ( ctx.unhandled ) {
       ctx.popState();

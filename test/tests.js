abbd6efafcb5b9700a49be70abd5dc7dd2b9860b
Gianluca Guarini <gianluca.guarini@gmail.com>
2014-11-30 12:43:38 +0100

    cleaning up the tests

--- test/tests.js
+++ test/tests.js
@@ -1,373 +1,377 @@
-var isNode = false,
-  $head,
-  $;
+/* globals before, after, chai, expect, page, describe, it */
+(function() {
 
-if (isNode = typeof window !== 'object') {
-  require('./support/jsdom');
+  'use strict';
+
+  var isNode = typeof window !== 'object',
+    global = this,
+    called = false,
+    html = '',
+    base = '',
+    hashbang = false,
+    chai = this.chai,
+    expect = this.expect,
+    page = this.page,
+    baseTag,
+    htmlWrapper,
+    $;
+
+  if (isNode) {
+    require('./support/jsdom');
+  }
 
   before(function() {
 
-    global.chai = require('chai');
-    global.expect = chai.expect;
-    global.page = process.env.PAGE_COV ? require('../index-cov') : require('../index');
-    $ = document.querySelector.bind(document);
-    $head = $('head');
-
-  });
-
-} else {
-  expect = chai.expect;
-  $ = document.querySelector.bind(document);
-  $head = $('head');
-}
-
-var called = false,
-  htmlWrapper,
-  html = '',
-  base = '',
-  baseTag,
-  hashbang = false,
-  /**
-   * Fire an event handler to the specified node. Event handlers can detect that the event was fired programatically
-   * by testing for a 'synthetic=true' property on the event object
-   * @param {HTMLNode} node The node to fire the event handler on.
-   * @param {String} eventName The name of the event without the "on" (e.g., "focus")
-   */
-  fireEvent = function(node, eventName) {
-
-    var event = document.createEvent('MouseEvents');
-
-    // https://developer.mozilla.org/en-US/docs/Web/API/event.initMouseEvent
-
-    event.initEvent(
-      eventName, true, true, window, 0,
-      event.screenX, event.screenY, event.clientX, event.clientY,
-      false, false, false, false,
-      0, null);
-
-    event.button = 1;
-    event.which = null;
-
-    node.dispatchEvent(event);
-
-  },
-  beforeTests = function(options) {
-    page.callbacks = [];
-    page.exits = [];
-    options = options || {};
-
-    page('/', function() {
-      called = true;
-    });
-
-    if (!baseTag) {
-      baseTag = document.createElement('base');
-      $head.appendChild(baseTag);
+    if (isNode) {
+      chai = require('chai');
+      expect = chai.expect;
+      page = process.env.PAGE_COV ? require('../index-cov') : require('../index');
+    } else {
+      expect = chai.expect;
     }
 
-    baseTag.setAttribute('href', (base ? base + '/' : '/'));
+    $ = document.querySelector.bind(document);
 
-    htmlWrapper = document.createElement('div');
+  });
 
-    html += '<ul class="links">';
-    html += '      <li><a class="index" href="./">/</a></li>';
-    html += '      <li><a class="whoop" href="#whoop">#whoop</a></li>';
-    html += '      <li><a class="about" href="./about">/about</a></li>';
-    html += '      <li><a class="contact" href="./contact">/contact</a></li>';
-    html += '      <li><a class="contact-me" href="./contact/me">/contact/me</a></li>';
-    html += '      <li><a class="not-found" href="./not-found?foo=bar">/not-found</a></li>';
-    html += '</ul>';
+  var fireEvent = function(node, eventName) {
 
-    htmlWrapper.innerHTML = html;
-    document.body.appendChild(htmlWrapper);
+      var event = document.createEvent('MouseEvents');
 
+      // https://developer.mozilla.org/en-US/docs/Web/API/event.initMouseEvent
+      event.initEvent(
+        eventName, true, true, this, 0,
+        event.screenX, event.screenY, event.clientX, event.clientY,
+        false, false, false, false,
+        0, null);
 
+      event.button = 1;
+      event.which = null;
 
-    page(options);
+      node.dispatchEvent(event);
 
-  },
-  tests = function() {
-    describe('on page load', function() {
-      it('should invoke the matching callback', function() {
-        expect(called).to.equal(true);
-      });
-    });
+    },
+    beforeTests = function(options) {
+      page.callbacks = [];
+      page.exits = [];
+      options = options || {};
 
-    describe('on redirect', function() {
-      it('should load destination page', function(done) {
-        page.redirect('/from', '/to');
-        page('/to', function() {
-          done();
-        });
-        page('/from');
-      });
-      it('should work with short alias', function(done) {
-        page('/one', '/two');
-        page('/two', function() {
-          done();
-        });
-        page('/one');
-      });
-      it('should load done within redirect', function(done) {
-        page('/redirect', function() {
-          page.redirect('/done');
-        });
-        page('/done', function() {
-          done();
-        });
-        page('/redirect');
-      });
-    });
-
-    describe('on exit', function() {
-      it('should run when exiting the page', function(done) {
-        page('/exit', function() {
-          visited = true;
-        });
-
-        page.exit('/exit', function() {
-          expect(visited).to.equal(true);
-          done();
-        });
-
-        page('/exit');
-        page('/');
+      page('/', function() {
+        called = true;
       });
 
-      it('should only run on matched routes', function(done) {
-        page('/should-exit', function() {});
-        page('/', function() {});
+      if (!baseTag) {
+        baseTag = document.createElement('base');
+        $('head').appendChild(baseTag);
+      }
 
-        page.exit('/should-not-exit', function() {
-          throw new Error('This exit route should not have been called');
+      baseTag.setAttribute('href', (base ? base + '/' : '/'));
+
+      htmlWrapper = document.createElement('div');
+
+      html += '<ul class="links">';
+      html += '      <li><a class="index" href="./">/</a></li>';
+      html += '      <li><a class="whoop" href="#whoop">#whoop</a></li>';
+      html += '      <li><a class="about" href="./about">/about</a></li>';
+      html += '      <li><a class="contact" href="./contact">/contact</a></li>';
+      html += '      <li><a class="contact-me" href="./contact/me">/contact/me</a></li>';
+      html += '      <li><a class="not-found" href="./not-found?foo=bar">/not-found</a></li>';
+      html += '</ul>';
+
+      htmlWrapper.innerHTML = html;
+      document.body.appendChild(htmlWrapper);
+
+
+
+      page(options);
+
+    },
+    tests = function() {
+      describe('on page load', function() {
+        it('should invoke the matching callback', function() {
+          expect(called).to.equal(true);
         });
-
-        page.exit('/should-exit', function() {
-          done();
-        });
-
-        page('/should-exit');
-        page('/');
       });
 
-      it('should use the previous context', function(done) {
-        var unique;
-
-        page('/', function() {});
-        page('/bootstrap', function(ctx) {
-          unique = ctx.unique = {};
+      describe('on redirect', function() {
+        it('should load destination page', function(done) {
+          page.redirect('/from', '/to');
+          page('/to', function() {
+            done();
+          });
+          page('/from');
         });
-
-        page.exit('/bootstrap', function(ctx) {
-          expect(ctx.unique).to.equal(unique);
-          done();
-        })
-
-        page('/bootstrap');
-        page('/');
-      });
-    });
-
-    describe('ctx.querystring', function() {
-      it('should default to ""', function(done) {
-        page('/querystring-default', function(ctx) {
-          expect(ctx.querystring).to.equal('');
-          done();
+        it('should work with short alias', function(done) {
+          page('/one', '/two');
+          page('/two', function() {
+            done();
+          });
+          page('/one');
         });
-
-        page('/querystring-default');
+        it('should load done within redirect', function(done) {
+          page('/redirect', function() {
+            page.redirect('/done');
+          });
+          page('/done', function() {
+            done();
+          });
+          page('/redirect');
+        });
       });
 
-      it('should expose the query string', function(done) {
-        page('/querystring', function(ctx) {
-          expect(ctx.querystring).to.equal('hello=there');
-          done();
+      describe('on exit', function() {
+        it('should run when exiting the page', function(done) {
+          var visited = false;
+          page('/exit', function() {
+            visited = true;
+          });
+
+          page.exit('/exit', function() {
+            expect(visited).to.equal(true);
+            done();
+          });
+
+          page('/exit');
+          page('/');
         });
 
-        page('/querystring?hello=there');
-      });
-    });
+        it('should only run on matched routes', function(done) {
+          page('/should-exit', function() {});
+          page('/', function() {});
 
-    describe('ctx.pathname', function() {
-      it('should default to ctx.path', function(done) {
-        page('/pathname-default', function(ctx) {
-          expect(ctx.pathname).to.equal(base + (base && hashbang ? '#!' : '') + '/pathname-default');
-          done();
+          page.exit('/should-not-exit', function() {
+            throw new Error('This exit route should not have been called');
+          });
+
+          page.exit('/should-exit', function() {
+            done();
+          });
+
+          page('/should-exit');
+          page('/');
         });
 
-        page('/pathname-default');
+        it('should use the previous context', function(done) {
+          var unique;
+
+          page('/', function() {});
+          page('/bootstrap', function(ctx) {
+            unique = ctx.unique = {};
+          });
+
+          page.exit('/bootstrap', function(ctx) {
+            expect(ctx.unique).to.equal(unique);
+            done();
+          });
+
+          page('/bootstrap');
+          page('/');
+        });
       });
 
-      it('should omit the query string', function(done) {
-        page('/pathname', function(ctx) {
-          expect(ctx.pathname).to.equal(base + (base && hashbang ? '#!' : '') + '/pathname');
-          done();
+      describe('ctx.querystring', function() {
+        it('should default to ""', function(done) {
+          page('/querystring-default', function(ctx) {
+            expect(ctx.querystring).to.equal('');
+            done();
+          });
+
+          page('/querystring-default');
         });
 
-        page('/pathname?hello=there');
-      });
-    });
+        it('should expose the query string', function(done) {
+          page('/querystring', function(ctx) {
+            expect(ctx.querystring).to.equal('hello=there');
+            done();
+          });
 
-    describe('ctx.handled', function() {
-      it('should skip unhandled redirect if exists', function() {
-        page('/page/:page', function(ctx, next) {
-          ctx.handled = true;
-          next();
+          page('/querystring?hello=there');
         });
-        var ctx = page.show('/page/1');
-        expect(ctx.handled).to.be.ok;
-      });
-    });
-
-    describe('links dispatcher', function() {
-
-      it('should invoke the callback', function(done) {
-        page('/about', function() {
-          done();
-        });
-        fireEvent($('.about'), 'click');
       });
 
-      it('should invoke the callback with the right params', function(done) {
-        page('/contact/:name', function(ctx) {
-          expect(ctx.params.name).to.equal('me');
-          done();
-        });
-        fireEvent($('.contact-me'), 'click');
-      });
+      describe('ctx.pathname', function() {
+        it('should default to ctx.path', function(done) {
+          page('/pathname-default', function(ctx) {
+            expect(ctx.pathname).to.equal(base + (base && hashbang ? '#!' : '') + '/pathname-default');
+            done();
+          });
 
-      it('should not invoke the callback', function() {
-        page('/whoop', function(ctx) {
-          expect(true).to.equal(false);
-        });
-        fireEvent($('.whoop'), 'click');
-      });
-
-    });
-
-
-    describe('dispatcher', function() {
-      it('should ignore query strings', function(done) {
-        page('/qs', function(ctx) {
-          done();
+          page('/pathname-default');
         });
 
-        page('/qs?test=true');
-      });
+        it('should omit the query string', function(done) {
+          page('/pathname', function(ctx) {
+            expect(ctx.pathname).to.equal(base + (base && hashbang ? '#!' : '') + '/pathname');
+            done();
+          });
 
-      it('should ignore query strings with params', function(done) {
-        page('/qs/:name', function(ctx) {
-          expect(ctx.params.name).to.equal('tobi');
-          done();
+          page('/pathname?hello=there');
         });
-
-        page('/qs/tobi?test=true');
       });
 
-      it('should invoke the matching callback', function(done) {
-        page('/user/:name', function(ctx) {
-          done();
-        });
-
-        page('/user/tj');
-      });
-
-      it('should populate ctx.params', function(done) {
-        page('/blog/post/:name', function(ctx) {
-          expect(ctx.params.name).to.equal('something');
-          done();
-        });
-
-        page('/blog/post/something');
-      });
-
-      describe('when next() is invoked', function() {
-        it('should invoke subsequent matching middleware', function(done) {
-          page('/forum/*', function(ctx, next) {
-            ctx.fullPath = ctx.params[0];
+      describe('ctx.handled', function() {
+        it('should skip unhandled redirect if exists', function() {
+          page('/page/:page', function(ctx, next) {
+            ctx.handled = true;
             next();
           });
-
-          page('/user', function() {
-
-          });
-
-          page('/forum/:fid/thread/:tid', function(ctx) {
-            expect(ctx.params.tid).to.equal('2');
-            done();
-          });
-
-          page('/forum/1/thread/2');
+          var ctx = page.show('/page/1');
+          expect(ctx.handled).to.be.ok;
         });
       });
 
+      describe('links dispatcher', function() {
 
-
-      describe('not found', function() {
-        it('should invoke the not found callback', function(done) {
-          page(function() {
+        it('should invoke the callback', function(done) {
+          page('/about', function() {
             done();
           });
-          page('/whathever');
+          fireEvent($('.about'), 'click');
         });
+
+        it('should invoke the callback with the right params', function(done) {
+          page('/contact/:name', function(ctx) {
+            expect(ctx.params.name).to.equal('me');
+            done();
+          });
+          fireEvent($('.contact-me'), 'click');
+        });
+
+        it('should not invoke the callback', function() {
+          page('/whoop', function(ctx) {
+            expect(true).to.equal(false);
+          });
+          fireEvent($('.whoop'), 'click');
+        });
+
       });
 
+
+      describe('dispatcher', function() {
+        it('should ignore query strings', function(done) {
+          page('/qs', function(ctx) {
+            done();
+          });
+
+          page('/qs?test=true');
+        });
+
+        it('should ignore query strings with params', function(done) {
+          page('/qs/:name', function(ctx) {
+            expect(ctx.params.name).to.equal('tobi');
+            done();
+          });
+
+          page('/qs/tobi?test=true');
+        });
+
+        it('should invoke the matching callback', function(done) {
+          page('/user/:name', function(ctx) {
+            done();
+          });
+
+          page('/user/tj');
+        });
+
+        it('should populate ctx.params', function(done) {
+          page('/blog/post/:name', function(ctx) {
+            expect(ctx.params.name).to.equal('something');
+            done();
+          });
+
+          page('/blog/post/something');
+        });
+
+        describe('when next() is invoked', function() {
+          it('should invoke subsequent matching middleware', function(done) {
+            page('/forum/*', function(ctx, next) {
+              ctx.fullPath = ctx.params[0];
+              next();
+            });
+
+            page('/user', function() {
+
+            });
+
+            page('/forum/:fid/thread/:tid', function(ctx) {
+              expect(ctx.params.tid).to.equal('2');
+              done();
+            });
+
+            page('/forum/1/thread/2');
+          });
+        });
+
+        describe('not found', function() {
+          it('should invoke the not found callback', function(done) {
+            page(function() {
+              done();
+            });
+            page('/whathever');
+          });
+        });
+
+
+
+      });
+    },
+    afterTests = function() {
+
+      document.body.removeChild(htmlWrapper);
+
+      called = false;
+      page.stop();
+      page.base('');
+      page('/');
+      base = '';
+
+    };
+
+  describe('Html5 history navigation', function() {
+
+    before(function() {
+      beforeTests();
     });
-  },
-  afterTests = function() {
 
-    document.body.removeChild(htmlWrapper);
+    tests();
 
-    called = false;
-    page.stop();
-    page.base('');
-    page('/');
-    base = '';
-  };
-
-describe('Html5 history navigation', function() {
-
-  before(function() {
-    beforeTests();
-  });
-
-  tests();
-
-  after(function() {
-    afterTests();
-  });
-
-});
-
-describe('Hashbang option enabled', function() {
-
-  before(function() {
-    hashbang = true;
-    beforeTests({
-      hashbang: hashbang
+    after(function() {
+      afterTests();
     });
+
   });
 
-  tests();
+  describe('Hashbang option enabled', function() {
 
-  after(function() {
-    afterTests();
+    before(function() {
+      hashbang = true;
+      beforeTests({
+        hashbang: hashbang
+      });
+    });
+
+    tests();
+
+    after(function() {
+      afterTests();
+    });
+
   });
 
-});
+  describe('Different Base', function() {
 
-describe('Different Base', function() {
+    before(function() {
+      base = '/newBase';
+      page.base(base);
+      beforeTests();
+    });
 
-  before(function() {
-    base = '/newBase';
-    page.base(base);
-    beforeTests();
+    tests();
+
+    after(function() {
+      afterTests();
+    });
+
   });
 
-  tests();
-
-  after(function() {
-    afterTests();
-  });
-
-});
+}).call(this);

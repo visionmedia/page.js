039cb4b05efb8b8cdc5c1e03f58cd30ae71f7b93
Henri Heiskanen <henri.heiskanen@gofore.com>
2014-12-05 10:33:58 +0200

    make URL decoding opt-out

--- index.js
+++ index.js
@@ -28,6 +28,11 @@
   var dispatch = true;
 
   /**
+  * Perform URL decoding.
+  */
+  var decode = true;
+
+  /**
    * Base path.
    */
 
@@ -134,6 +139,7 @@
     if (running) return;
     running = true;
     if (false === options.dispatch) dispatch = false;
+    if (false === options.decode) decode = false;
     if (false !== options.popstate) window.addEventListener('popstate', onpopstate, false);
     if (false !== options.click) window.addEventListener('click', onclick, false);
     if (true === options.hashbang) hashbang = true;
@@ -307,7 +313,7 @@
    * @param {str} URL component to decode
    */
   function decodeURLEncodedURIComponent(str) {
-    return decodeURIComponent(str.replace(/\+/g, ' '));
+    return decode ? decodeURIComponent(str.replace(/\+/g, ' ')) : str;
   }
 
   /**

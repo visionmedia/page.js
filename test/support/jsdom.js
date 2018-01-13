69026bcde79ebf801812608d7a099e1f61787fab
Matthew Phillips <matthew@matthewphillips.info>
2018-01-13 11:57:52 -0500

    Allow development in more recent Node versions
    This makes it possible to develop page.js using more recent Node
    versions, while staying backwards compatible to 0.10 for CI purposes
    mostly.
    
    Closes #432

--- test/support/jsdom.js
+++ test/support/jsdom.js
@@ -1,19 +1,34 @@
 before(function(next) {
-  require('jsdom').env({
-    html: '<!doctype html><html><head></head><body></body></html>',
-    done: function(errors, window) {
-      window.console = console;
-      window.history.replaceState(null, '', '/');
-      global.window = window;
-      global.location = window.location;
-      global.document = window.document;
-      global.history = window.history;
-      window._$jscoverage = global._$jscoverage;
-      if (errors) {
-        errors.forEach(console.log);
-        throw new Error(errors[0].data.error);
+  var jsdom = require('jsdom');
+  var html = '<!doctype html><html><head></head><body></body></html>';
+
+  function setupGlobals(window) {
+    window.console = console;
+    window.history.replaceState(null, '', '/');
+    global.window = window;
+    global.location = window.location;
+    global.document = window.document;
+    global.history = window.history;
+    window._$jscoverage = global._$jscoverage;
+  }
+
+  if(jsdom.env) {
+    jsdom.env({
+      html: html,
+      done: function(errors, window) {
+        setupGlobals(window);
+        if (errors) {
+          errors.forEach(console.log);
+          throw new Error(errors[0].data.error);
+        }
+        next();
       }
-      next();
-    }
-  });
+    });
+  } else {
+    var dom = new jsdom.JSDOM(html, {
+      url: 'http://example.com'
+    });
+    setupGlobals(dom.window);
+    next();
+  }
 });

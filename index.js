1700d5724fea8077530b7deb152371a7017a1e22
Mykhailo Nakonechnyi <mykhailo.nakonechnyi@dreamhost.com>
2018-06-05 05:12:49 -0700

    Valera's changes

--- index.js
+++ index.js
@@ -210,9 +210,9 @@
       var loc = pageWindow.location;
 
       if(hashbang && ~loc.hash.indexOf('#!')) {
-        url = loc.hash.substr(2) + loc.search;
+        url = loc.hash.substr(2);
       } else if (hashbang) {
-        url = loc.search + loc.hash;
+        url = loc.hash;
       } else {
         url = loc.pathname + loc.search + loc.hash;
       }
@@ -593,6 +593,9 @@
       if (e.state) {
         var path = e.state.path;
         page.replace(path, e.state);
+      } else if (isLocation && hashbang) {
+        var loc = pageWindow.location;
+        page.show(loc.hash, undefined, undefined, false);
       } else if (isLocation) {
         var loc = pageWindow.location;
         page.show(loc.pathname + loc.hash, undefined, undefined, false);
